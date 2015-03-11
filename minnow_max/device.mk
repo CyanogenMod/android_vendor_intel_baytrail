14f5434e6a489d0488963151ae68431c58a5b516
Mihai Serban <mihai.serban@intel.com>
2015-03-11 13:24:11 +0200

    mixin-update: ART: Include libartd-extension plugin libraries
    Change-Id: I78cd1364af9d4921b6003e90dda2440d903d87c3
    Signed-off-by: Mihai Serban <mihai.serban@intel.com>
    Signed-off-by: Petre Pircalabu <petre.ovidiu.pircalabu@intel.com>

--- minnow_max/device.mk
+++ minnow_max/device.mk
@@ -336,5 +336,5 @@ endif
 # This is needed to enable silver art optimizer.
 # This will build the plugins/libart-extension.so library,  which is dynamically loaded by
 # AOSP and contains Intel optimizations to the compiler.
-PRODUCT_PACKAGES += libart-extension
+PRODUCT_PACKAGES += libart-extension libartd-extension
 # ------------------ END MIX-IN DEFINITIONS ------------------
